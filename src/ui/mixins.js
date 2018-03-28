d9b6fe663f3ef016eb7f36e4f99d02049e9316fc
Zelofff <zelofflab@gmail.com>
2018-03-28 20:57:18 +0300

    Fixed prettier pre-commit hook

--- src/ui/mixins.js
+++ src/ui/mixins.js
@@ -1,14 +1,14 @@
 export const breakpoints = {
-    up: {
-        xs: 480,
-        sm: 768,
-        md: 1024,
-        lg: 1280,
-    },
-    down: {
-        xs: 479,
-        sm: 767,
-        md: 1023,
-        lg: 1279,
-    },
+  up: {
+    xs: 480,
+    sm: 768,
+    md: 1024,
+    lg: 1280,
+  },
+  down: {
+    xs: 479,
+    sm: 767,
+    md: 1023,
+    lg: 1279,
+  },
 };
