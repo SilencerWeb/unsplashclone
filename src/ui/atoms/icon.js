79c5aa0dc6706ea9536ebfe2d272c128baae29dd
SilencerWeb <silencer.web@gmail.com>
2018-03-29 05:55:21 +0500

    Add scripts flow and deploy in package.json; Add gh-pages

--- src/ui/atoms/icon.js
+++ src/ui/atoms/icon.js
@@ -6,19 +6,21 @@ const SVG = styled.svg`
   display: inline-block;
   vertical-align: top;
 
-  ${p => p.icon && css`
-    width: ${p.icon.node.viewBox.animVal.width / p.icon.node.viewBox.animVal.height}em;
-    height: 1em;
-    font-size: ${p.height || p.icon.node.viewBox.animVal.height}px;
-  `};
-  
+  ${p =>
+    p.icon &&
+    css`
+      width: ${p.icon.node.viewBox.animVal.width / p.icon.node.viewBox.animVal.height}em;
+      height: 1em;
+      font-size: ${p.height || p.icon.node.viewBox.animVal.height}px;
+    `};
+
   fill: currentColor;
 `;
 
-export const Icon = (props) => {
+export const Icon = props => {
   return (
-    <SVG className={ props.className }>
-      <use xlinkHref={ '#' + props.icon.id }/>
+    <SVG className={props.className}>
+      <use xlinkHref={'#' + props.icon.id} />
     </SVG>
   );
 };
